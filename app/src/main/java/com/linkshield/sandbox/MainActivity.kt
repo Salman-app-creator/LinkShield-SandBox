b88b3eda4560f0f69365f38c99b019693b114356
Salman-app-creator <salmanlatif227@gmail.com>
2026-08-07 21:10:06 +0500

    Add button to open app settings in MainActivity

--- app/src/main/java/com/linkshield/sandbox/MainActivity.kt
+++ app/src/main/java/com/linkshield/sandbox/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.linkshield.sandbox
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +13,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnEnable: Button = findViewById(R.id.btnEnableProtection)
+        btnEnable.setOnClickListener {
+            openDefaultAppSettings()
+        }
+    }
+
+    private fun openDefaultAppSettings() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val intent = Intent(
+                Settings.ACTION_APP_OPEN_BY_DEFAULT_SETTINGS,
+                Uri.parse("package:$packageName")
+            )
+            startActivity(intent)
+        } else {
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            intent.data = Uri.parse("package:$packageName")
+            startActivity(intent)
+        }
     }
 }
