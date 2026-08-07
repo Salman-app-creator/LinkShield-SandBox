99c65a93d8fc12ccd1e09cbbfb92efa55542495b
Salman-app-creator <salmanlatif227@gmail.com>
2026-08-07 16:39:58 +0500

    Update MainActivity.kt

--- app/src/main/java/com/linkshield/sandbox/MainActivity.kt
+++ app/src/main/java/com/linkshield/sandbox/MainActivity.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Dashboard logic
+        setContentView(R.layout.activity_main)
     }
 }
