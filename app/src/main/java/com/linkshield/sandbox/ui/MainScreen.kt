fdc835bb725852023b0b0dcf9ff266fa570164d3
Salman Latif <salmanlatif227@gmail.com>
2026-08-15 10:17:46 +0500

    Update MainScreen.kt

--- app/src/main/java/com/linkshield/sandbox/ui/MainScreen.kt
+++ app/src/main/java/com/linkshield/sandbox/ui/MainScreen.kt
@@ -1,45 +1,50 @@
 package com.linkshield.sandbox.ui
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import com.linkshield.sandbox.ui.components.BottomNavigationBar
 import com.linkshield.sandbox.ui.components.TopHeader
 import com.linkshield.sandbox.ui.screens.GrabberScreen
-import com.linkshield.sandbox.ui.screens.OnboardingScreen
+import com.linkshield.sandbox.ui.screens.ShieldScreen
 import com.linkshield.sandbox.ui.screens.UpgradeScreen
 
 @Composable
-fun MainScreen() {
-    var selectedTab by remember { mutableStateOf(1) } // Default: Grabber Tab
-    var isDarkTheme by remember { mutableStateOf(true) }
-    var showOnboarding by remember { mutableStateOf(false) }
+fun MainScreen(
+    isDarkTheme: Boolean,
+    onThemeToggle: () -> Unit
+) {
+    var selectedTab by remember { mutableIntStateOf(0) }
+    var currentUrl by remember { mutableStateOf("https://www.google.com/") }
+    var isShieldActive by remember { mutableStateOf(true) }
+    var trialDaysLeft by remember { mutableIntStateOf(30) }
 
-    if (showOnboarding) {
-        OnboardingScreen(onGetStarted = { showOnboarding = false })
-    } else {
-        Scaffold(
-            topBar = {
-                TopHeader(
-                    selectedTab = selectedTab,
-                    onTabSelected = { selectedTab = it },
-                    isDarkTheme = isDarkTheme,
-                    onThemeToggle = { isDarkTheme = !isDarkTheme }
-                )
-            }
-        ) { innerPadding ->
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(innerPadding)
-            ) {
-                when (selectedTab) {
-                    0 -> { /* Sandbox Browser Engine yahan aayega */ }
-                    1 -> GrabberScreen()
-                    2 -> UpgradeScreen()
-                }
+    Scaffold(
+        topBar = {
+            TopHeader(
+                currentUrl = currentUrl,
+                onUrlChange = { currentUrl = it },
+                isShieldActive = isShieldActive,
+                onShieldToggle = { isShieldActive = !isShieldActive },
+                trialDaysLeft = trialDaysLeft,
+                isDarkTheme = isDarkTheme,
+                onThemeToggle = onThemeToggle
+            )
+        },
+        bottomBar = {
+            BottomNavigationBar(
+                selectedTab = selectedTab,
+                onTabSelected = { selectedTab = it }
+            )
+        }
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            when (selectedTab) {
+                0 -> ShieldScreen(currentUrl = currentUrl)
+                1 -> GrabberScreen()
+                2 -> UpgradeScreen()
             }
         }
     }
