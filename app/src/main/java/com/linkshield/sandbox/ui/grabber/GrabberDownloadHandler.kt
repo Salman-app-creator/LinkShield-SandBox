7511e99eec0bf1b952f17657dd8b574f3f716d6c
Salman Latif <salmanlatif227@gmail.com>
2026-08-16 22:11:04 +0500

    Refactor download method to return download ID
    Removed Toast notifications and simplified download method.

--- app/src/main/java/com/linkshield/sandbox/ui/grabber/GrabberDownloadHandler.kt
+++ app/src/main/java/com/linkshield/sandbox/ui/grabber/GrabberDownloadHandler.kt
@@ -1,7 +1,6 @@
 package com.linkshield.sandbox.ui.grabber
 
 import android.content.Context
-import android.widget.Toast
 
 class GrabberDownloadHandler(
     context: Context
@@ -14,23 +13,7 @@ class GrabberDownloadHandler(
 
     fun download(
         option: MediaQualityOption
-    ) {
-        val id =
-            manager.download(option)
-
-        if (id == null) {
-            Toast.makeText(
-                context,
-                "Unable to start download",
-                Toast.LENGTH_SHORT
-            ).show()
-            return
-        }
-
-        Toast.makeText(
-            context,
-            "Download started",
-            Toast.LENGTH_SHORT
-        ).show()
+    ): Long? {
+        return manager.download(option)
     }
 }
