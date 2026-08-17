1f64b0a04a7cb3139f857b91e2e62203a1761d1a
Salman Latif <salmanlatif227@gmail.com>
2026-08-17 22:43:44 +0500

    Add ExperimentalMaterial3Api annotation to GrabberScreen

--- app/src/main/java/com/linkshield/sandbox/ui/grabber/GrabberScreen.kt
+++ app/src/main/java/com/linkshield/sandbox/ui/grabber/GrabberScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
@@ -31,6 +32,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GrabberScreen(
     viewModel: GrabberViewModel,
@@ -223,7 +225,9 @@ fun GrabberScreen(
                     Text(
                         text = message,
                         color =
-                            MaterialTheme.colorScheme.error
+                            MaterialTheme
+                                .colorScheme
+                                .error
                     )
                 }
             }
