9c4d0369d0b12de8a9a5bd60000d171710060ab6
Salman Latif <salmanlatif227@gmail.com>
2026-08-18 00:15:06 +0500

    Refactor MainActivity to use LinkShieldApp composable

--- app/src/main/java/com/linkshield/sandbox/MainActivity.kt
+++ app/src/main/java/com/linkshield/sandbox/MainActivity.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.linkshield.sandbox.ui.browser.SandboxBrowserScreen
-import com.linkshield.sandbox.ui.grabber.MediaGrabberScreen
-import com.linkshield.sandbox.ui.grabber.MediaExtractorViewModel
 import com.linkshield.sandbox.ui.theme.LinkShieldSandboxTheme
 import com.linkshield.sandbox.ui.unblock.UnblockShieldScreen
 import com.linkshield.sandbox.ui.unblock.UnblockShieldViewModel
@@ -20,17 +17,21 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LinkShieldSandboxTheme {
-                val unblockViewModel: UnblockShieldViewModel = viewModel()
-                val mediaExtractorViewModel: MediaExtractorViewModel = viewModel()
+            LinkShieldApp()
+        }
+    }
+}
 
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    UnblockShieldScreen(viewModel = unblockViewModel)
-                }
-            }
+@Composable
+fun LinkShieldApp() {
+    LinkShieldSandboxTheme {
+        val unblockViewModel: UnblockShieldViewModel = viewModel()
+
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            UnblockShieldScreen(viewModel = unblockViewModel)
         }
     }
 }
