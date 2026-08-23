eefa260c945579e6b48244fb9fe40fba5e739908
Salman Latif <salmanlatif227@gmail.com>
2026-08-23 20:10:55 +0500

    Remove unused import in TopHeader.kt
    Removed unused import for SolidColor in TopHeader.kt

--- app/src/main/java/com/linkshield/sandbox/ui/components/TopHeader.kt
+++ app/src/main/java/com/linkshield/sandbox/ui/components/TopHeader.kt
@@ -30,7 +30,6 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.linkshield.sandbox.R
-import androidx.compose.ui.graphics.SolidColor
 
 @Composable
 fun TopHeader(
