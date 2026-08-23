88b5ed1585b09613e521fab4649e61e8bce9487e
Salman Latif <salmanlatif227@gmail.com>
2026-08-23 18:30:59 +0500

    Add weight modifier to SandboxBrowserScreen layout

--- app/src/main/java/com/linkshield/sandbox/ui/browser/SandboxBrowserScreen.kt
+++ app/src/main/java/com/linkshield/sandbox/ui/browser/SandboxBrowserScreen.kt
@@ -10,6 +10,7 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.weight
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
