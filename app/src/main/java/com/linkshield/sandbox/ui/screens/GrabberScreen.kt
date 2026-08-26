fefbf44fb671b931abdf59eb0342a90574d477de
Salman Latif <salmanlatif227@gmail.com>
2026-08-26 20:02:45 +0500

    Refactor GrabberScreen to use BrowserViewModel

--- app/src/main/java/com/linkshield/sandbox/ui/screens/GrabberScreen.kt
+++ app/src/main/java/com/linkshield/sandbox/ui/screens/GrabberScreen.kt
@@ -1,10 +1,23 @@
 package com.linkshield.sandbox.ui.screens
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.linkshield.sandbox.ui.browser.BrowserViewModel
 import com.linkshield.sandbox.ui.grabber.LinkShieldGrabberScreen
 
 @Composable
-fun GrabberScreen() {
-    LinkShieldGrabberScreen(onBackToBrowser = {}, onUpgradeClick = {})
+fun GrabberScreen(
+    browserViewModel: BrowserViewModel,
+    onBackToBrowser: () -> Unit = {},
+    onUpgradeClick: () -> Unit = {}
+) {
+    val activeUrl by browserViewModel.currentUrl.collectAsState()
+
+    LinkShieldGrabberScreen(
+        initialUrl = activeUrl,
+        onBackToBrowser = onBackToBrowser,
+        onUpgradeClick = onUpgradeClick
+    )
 }
