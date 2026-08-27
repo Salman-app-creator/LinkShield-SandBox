1cf6bbdec40d24e5f581192a5b5a3698461e4b93
Salman Latif <salmanlatif227@gmail.com>
2026-08-27 08:10:41 +0500

    Change BrowserViewModel to SandboxBrowserViewModel

--- app/src/main/java/com/linkshield/sandbox/ui/screens/GrabberScreen.kt
+++ app/src/main/java/com/linkshield/sandbox/ui/screens/GrabberScreen.kt
@@ -1,22 +1,23 @@
 package com.linkshield.sandbox.ui.screens
 
+// REPO PATH: app/src/main/java/com/linkshield/sandbox/ui/screens/GrabberScreen.kt
+
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import com.linkshield.sandbox.ui.browser.BrowserViewModel
+import com.linkshield.sandbox.ui.browser.SandboxBrowserViewModel
 import com.linkshield.sandbox.ui.grabber.LinkShieldGrabberScreen
 
 @Composable
 fun GrabberScreen(
-    browserViewModel: BrowserViewModel,
+    browserViewModel: SandboxBrowserViewModel,
     onBackToBrowser: () -> Unit = {},
     onUpgradeClick: () -> Unit = {}
 ) {
-    val activeUrl by browserViewModel.currentUrl.collectAsState()
+    val uiState by browserViewModel.uiState.collectAsState()
 
     LinkShieldGrabberScreen(
-        initialUrl = activeUrl,
+        initialUrl = uiState.url,
         onBackToBrowser = onBackToBrowser,
         onUpgradeClick = onUpgradeClick
     )
